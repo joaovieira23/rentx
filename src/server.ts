43f6a150022801afc8cd581cfeffb8e53580cb71
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-14 12:02:19 -0300

    feat: create category creation documentation

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup())
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router);
 
