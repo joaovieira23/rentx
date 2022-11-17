4d5d051a98164273fb1c666b0cb71cb67fc281fc
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-17 10:35:59 -0300

    feat: create user token

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,61 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+
+interface IRequest {
+    email: string;
+    password: string;
+};
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+};
+
+class AuthenticateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ){}
+
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        // Verificar se o usuário existe
+        const user = await this.usersRepository.findByEmail(email);
+
+        if(!user) {
+            throw new Error("Email or password incorret!");
+        };
+
+        // Verificar se a senha está correta
+
+        const passwordMatch = await compare(password, user.password);
+
+        if(!passwordMatch) {
+            throw new Error("Email or password incorret!");
+        };
+
+        // Gerar o JWT
+
+        const token = sign({}, "ec8225af0776b3ee5a28678986159226", {
+            subject: user.id,
+            expiresIn: "1d",
+        });
+
+       const tokenReturn: IResponse = {
+            user: {
+               name: user.name,
+               email: user.email
+            },
+            token
+       }; 
+
+        return tokenReturn;
+    }
+};
+
+export { AuthenticateUserUseCase };
