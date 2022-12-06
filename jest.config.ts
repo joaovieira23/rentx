d444eb6ef458d4a5fde5e18eb4479575c2abcbb6
João Andrade <joaovictorvieira.23@hotmail.com>
2022-12-06 16:52:00 -0300

    fix: imports

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,4 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
+import { pathsToModuleNameMapper } from 'ts-jest';
 
 export default {
   // All imported modules in your tests should be mocked automatically
@@ -88,7 +85,15 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: pathsToModuleNameMapper({
+    "@modules/*": ["modules/*"],
+    "@config/*": ["config/*"],
+    "@shared/*": ["shared/*"],
+    "@errors/*": ["errors/*"],
+    "@utils/*": ["utils/*"],
+  }, {
+    prefix: "<rootDir>/src/"
+  }),
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
